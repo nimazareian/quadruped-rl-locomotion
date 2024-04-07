7e1f65d5837515b969af1e6f8cbf6a52f0a242ab
parsa.bakhshandeh@sanctuary.ai <parsa.bakhshandeh@sanctuary.ai>
2024-04-07 13:45:44 -0700

    fix: obs not updating in each step

--- loco_mujoco_test_env.py
+++ loco_mujoco_test_env.py
@@ -43,14 +43,14 @@ def train(args):
 
     model = PPO("MlpPolicy", vec_env, verbose=1, tensorboard_log=LOG_DIR)
     train_time = time.strftime("%Y-%m-%d_%H-%M-%S")
-    model.learn(total_timesteps=args.time_steps_per_save, reset_num_timesteps=False)
-    model.save(f"{MODEL_DIR}/{train_time}/ppo_loco_mujoco_{args.time_steps_per_save}")
+    model.learn(total_timesteps=args.total_timesteps)
+    model.save(f"{MODEL_DIR}/{train_time}/ppo_loco_mujoco_{args.total_timesteps}")
     del model
-    model = PPO.load(f"{MODEL_DIR}/{train_time}/ppo_loco_mujoco_{args.time_steps_per_save}")
+    model = PPO.load(f"{MODEL_DIR}/{train_time}/ppo_loco_mujoco_{args.total_timesteps}")
 
 
 def test(args):
-    env = gym.make(args.env, render_mode="human")
+    env = make_vec_env(args.env, n_envs=1)
     # env = make_vec_env(args.env, n_envs=4)
     
     model = PPO.load(path=args.model_path)
@@ -59,17 +59,17 @@ def test(args):
     episode_reward = 0
     episode_length = 0
     for _ in range(NUM_EPISODES):
-        obs, _ = env.reset()
+        obs = env.reset()
         env.render()
         while True:
             action, _ = model.predict(obs)
-            nstate, reward, terminated, truncated, info = env.step(action)
+            obs, reward, terminated, info = env.step(action)
             episode_reward += reward
-            env.render()
-            time.sleep(0.1)
+            env.render("human")
+            # time.sleep(0.01)
             episode_length += 1
 
-            if terminated:
+            if terminated[0]:
                 break
 
     print(f"Total episode reward: {episode_reward}, avg episode length: {episode_length / NUM_EPISODES}")
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     parser.add_argument("--env", type=str, default="CartPole-v1", help="Environment to train/test on")
     parser.add_argument("--seed", type=float, default=0)
     parser.add_argument("--num_parallel_envs", type=int, default=4)
-    parser.add_argument("--time_steps_per_save", type= int, default=100000)
+    parser.add_argument("--total_timesteps", type= int, default=100000)
     parser.add_argument("--existing_model_path", type= float, default=None, help="Path to the model to continue training")
     parser.add_argument("--model_path", type= str, default=None, help="Path to the model to test")
     args = parser.parse_args()
